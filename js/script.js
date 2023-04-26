f481b9c5b8c47fd5c2ba27b8663e5c6022c01877
himyson <129506955+himyson@users.noreply.github.com>
2023-04-26 13:26:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,103 +1,406 @@
-// у вас есть 3 вопроса 
-// спрашивает ваше имя
-// фамилию 
-// женаты ли вы 
-// вы создать объекь с этими значениями
-
-
-
-let n = prompt('Ваше имя')
-let s = prompt('Фамилия')
-let m = confirm('Женаты ли вы?')
-let ilon = {
-    name: n,
-    surname: s,
-    ismarried: m
-}
-console.log(ilon);
-
-
-// let a = 2                  a = 3
-// let b = 3                  b = 2
-// поменяйте значения а и b местами
-// поменяйте значения а и b местами 
-// не используя дополнительных переменых
-
-
-let a = 3
-let b = 2
-console.log(a, b);
-a = b, ([b=a][0])
-console.log(a);
-
-
-// из банковской системы создать 2 аккаунта 
-// 2 объекта
-// и сделать банкувскую систему на 2 человек
-
-
-let account_1 = prompt('Name')
-let balance_1 = 30000
-let account_code_1 = 1111
-if(account_1 == 'Rokki'){
-    let account_code_1 = prompt('Code')
-    if(account_code_1 == 1111){
-        let show_1 = alert('Your balance is 30000')
-        let account_money_1 = prompt('How much money want you get?')
-        if(account_money_1 <= 30000){
-            let result_1 = alert('Everything went well!')
-            let account_balance_1 = confirm('Do you want to see the balance?')
-            if(account_balance_1 == true){
-                alert(`You have ${balance_1 - account_money_1}`)
-            }
-        }else{
-            alert('Lack of money bro')
+// Раскидать людей в разные массивы в зависимости от их почты
+let users = [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
         }
-    }else{
-        alert('Incorrect code')
-    }
-}else{
-    alert('undefined')
-}
-
-
-
-let account_2 = prompt('Name')
-let balance_2 = 50000
-let account_code_2 = 6666
-if(account_2 == 'Vandam'){
-    let account_code_2 = prompt('Code')
-    if(account_code_2 == 6666){
-        let show_2 = alert('Your balance is 50000')
-        let account_money_2 = prompt('How much money want you get?')
-        if(account_money_2 <= 50000){
-            let result_2 = alert('Everything went well!')
-            let account_balance_2 = confirm('Do you want to see the balance?')
-            if(account_balance_2 == true){
-                alert(`You have ${balance_2 - account_money_2}`)
-            }
-        }else{
-            alert('Lack of money bro')
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+      }
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+          "lat": "-43.9509",
+          "lng": "-34.4618"
         }
-    }else{
-        alert('Incorrect code')
+      },
+      "phone": "010-692-6593 x09125",
+      "website": "anastasia.net",
+      "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+      }
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "address": {
+        "street": "Douglas Extension",
+        "suite": "Suite 847",
+        "city": "McKenziehaven",
+        "zipcode": "59590-4157",
+        "geo": {
+          "lat": "-68.6102",
+          "lng": "-47.0653"
+        }
+      },
+      "phone": "1-463-123-4447",
+      "website": "ramiro.info",
+      "company": {
+        "name": "Romaguera-Jacobson",
+        "catchPhrase": "Face to face bifurcated interface",
+        "bs": "e-enable strategic applications"
+      }
+    },
+    {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+      "address": {
+        "street": "Hoeger Mall",
+        "suite": "Apt. 692",
+        "city": "South Elvis",
+        "zipcode": "53919-4257",
+        "geo": {
+          "lat": "29.4572",
+          "lng": "-164.2990"
+        }
+      },
+      "phone": "493-170-9623 x156",
+      "website": "kale.biz",
+      "company": {
+        "name": "Robel-Corkery",
+        "catchPhrase": "Multi-tiered zero tolerance productivity",
+        "bs": "transition cutting-edge web services"
+      }
+    },
+    {
+      "id": 5,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "email": "Lucio_Hettinger@annie.ca",
+      "address": {
+        "street": "Skiles Walks",
+        "suite": "Suite 351",
+        "city": "Roscoeview",
+        "zipcode": "33263",
+        "geo": {
+          "lat": "-31.8129",
+          "lng": "62.5342"
+        }
+      },
+      "phone": "(254)954-1289",
+      "website": "demarco.info",
+      "company": {
+        "name": "Keebler LLC",
+        "catchPhrase": "User-centric fault-tolerant solution",
+        "bs": "revolutionize end-to-end systems"
+      }
+    },
+    {
+      "id": 6,
+      "name": "Mrs. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "email": "Karley_Dach@jasper.info",
+      "address": {
+        "street": "Norberto Crossing",
+        "suite": "Apt. 950",
+        "city": "South Christy",
+        "zipcode": "23505-1337",
+        "geo": {
+          "lat": "-71.4197",
+          "lng": "71.7478"
+        }
+      },
+      "phone": "1-477-935-8478 x6430",
+      "website": "ola.org",
+      "company": {
+        "name": "Considine-Lockman",
+        "catchPhrase": "Synchronised bottom-line interface",
+        "bs": "e-enable innovative applications"
+      }
+    },
+    {
+      "id": 7,
+      "name": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "email": "Telly.Hoeger@billy.biz",
+      "address": {
+        "street": "Rex Trail",
+        "suite": "Suite 280",
+        "city": "Howemouth",
+        "zipcode": "58804-1099",
+        "geo": {
+          "lat": "24.8918",
+          "lng": "21.8984"
+        }
+      },
+      "phone": "210.067.6132",
+      "website": "elvis.io",
+      "company": {
+        "name": "Johns Group",
+        "catchPhrase": "Configurable multimedia task-force",
+        "bs": "generate enterprise e-tailers"
+      }
+    },
+    {
+      "id": 8,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "email": "Sherwood@rosamond.me",
+      "address": {
+        "street": "Ellsworth Summit",
+        "suite": "Suite 729",
+        "city": "Aliyaview",
+        "zipcode": "45169",
+        "geo": {
+          "lat": "-14.3990",
+          "lng": "-120.7677"
+        }
+      },
+      "phone": "586.493.6943 x140",
+      "website": "jacynthe.com",
+      "company": {
+        "name": "Abernathy Group",
+        "catchPhrase": "Implemented secondary concept",
+        "bs": "e-enable extensible e-tailers"
+      }
+    },
+    {
+      "id": 9,
+      "name": "Glenna Reichert",
+      "username": "Delphine",
+      "email": "Chaim_McDermott@dana.io",
+      "address": {
+        "street": "Dayna Park",
+        "suite": "Suite 449",
+        "city": "Bartholomebury",
+        "zipcode": "76495-3109",
+        "geo": {
+          "lat": "24.6463",
+          "lng": "-168.8889"
+        }
+      },
+      "phone": "(775)976-6794 x41206",
+      "website": "conrad.com",
+      "company": {
+        "name": "Yost and Sons",
+        "catchPhrase": "Switchable contextually-based project",
+        "bs": "aggregate real-time technologies"
+      }
+    },
+    {
+      "id": 10,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "email": "Rey.Padberg@karina.biz",
+      "address": {
+        "street": "Kattie Turnpike",
+        "suite": "Suite 198",
+        "city": "Lebsackbury",
+        "zipcode": "31428-2261",
+        "geo": {
+          "lat": "-38.2386",
+          "lng": "57.2232"
+        }
+      },
+      "phone": "024-648-3804",
+      "website": "ambrose.net",
+      "company": {
+        "name": "Hoeger LLC",
+        "catchPhrase": "Centralized empowering task-force",
+        "bs": "target end-to-end models"
+      }
     }
-}else{
-    alert('undefined')
-}
-
-
-// let a = {price: 30}
-// let b = {price: -40}
-// let c = {price: 50}
-// объеденить и посмотреть ответ
-// в переменую тотал сложить все прайсы и дать ответ
-
-
-let q = {price: 30}
-let w = {price: -40}
-let e = {price: 50}
-let total = Object.assign(q.price + w.price + e.price)
-console.log(total);
-
-
+   ]
+  let emails = {
+    org: [],
+    net: [],
+    info: []
+  }
+  let other = []
+  for(let item of users){
+    if(item.email.includes('org') === true){
+    emails.org.push(item)
+    }else if(item.email.includes('net') === true){
+      emails.net.push(item)
+    }else if(item.email.includes('info') === true){
+      emails.info.push(item)
+    }else{
+      other.push(item)
+    }
+  }
+  console.log(emails);
+  console.log(other);
+  
+  
+  let arr3 = [1, 'hello', true, 2, 3, 'world', undefined, null, 'error', 22];
+  
+  let iter = 0
+  let n = 0 
+  for(let item of arr3){
+     iter++
+  
+    if(typeof(item) === 'number'){
+      n+=1 
+    }
+  }
+  if(n >5){
+  console.log('good');  
+  }else{
+    console.log('bad');
+  }
+    console.log(iter);
+  console.log(n);
+  
+  
+  let arr = [
+      {
+          id: Math.random(),
+          name: 'Timur',
+          info: {
+              school: '235',
+              faculity: 'SMM'
+          },
+      },
+      {
+          id: Math.random(),
+          name: 'Imran',
+          info: {
+              school: 'ne izvestno',
+              faculity: 'programming'
+          },
+      },
+      {
+          id: Math.random(),
+          name: 'Aminjon',
+          info: {
+              school: '444',
+              faculity: 'Dizayn'
+          },
+      },
+      {
+          id: Math.random(),
+          name: 'Maxmud',
+          info: {
+              school: '777',
+              faculity: '3dsmax'
+          },
+      },
+      {
+          id: Math.random(),
+          name: 'Muxammad',
+          info: {
+              school: '5555',
+              faculity: 'Backend'
+          },
+      },
+      {
+          id: Math.random(),
+          name: 'Timur',
+          info: {
+              school: '235',
+              faculity: 'SMM'
+          },
+      },
+      {
+          id: Math.random(),
+          name: 'Imran',
+          info: {
+              school: 'ne izvestno',
+              faculity: 'programming'
+          },
+      },
+      {
+          id: Math.random(),
+          name: 'Aminjon',
+          info: {
+              school: '444',
+              faculity: 'Dizayn'
+          },
+      },
+      {
+          id: Math.random(),
+          name: 'Maxmud',
+          info: {
+              school: '777',
+              faculity: '3dsmax'
+          },
+      },
+      {
+          id: Math.random(),
+          name: 'Maxmud',
+          info: {
+              school: '777',
+              faculity: '3dsmax'
+          },
+      },
+      {
+          id: Math.random(),
+          name: 'Muxammad',
+          info: {
+              school: '5555',
+              faculity: 'Backend'
+          },
+      },
+  ]
+  
+  let categories = [
+      {
+          course: 'SMM',
+          count: 0
+      },
+      {
+          course: 'PROGRAMMING',
+          count: 0
+      },
+      {
+          course: '3DSMAX',
+          count: 0
+      },
+      {
+          course: 'DIZAYN',
+          count: 0
+      },
+      {       
+          course: 'BACKEND',
+          count: 0
+      },
+  ]
+  
+  // Описание задачи 
+  
+  // Найти количество людей одного типа из массива и сохранить их в массив категории 
+  // В массиве категорий специально допущены ошибки (исходный массив менять нельзя)
+  
+   
+  for(let item of arr) {
+    let faculity = item.info.faculity.toLocaleLowerCase().trim()
+    for(let element of categories){
+      let elem = element.course.toLocaleLowerCase().trim()
+      if (faculity === elem) {
+          element.count++
+      }
+    }
+  }
+  console.log(categories);
+  
+  
